3178503f2f6a61a69e0331aabe2ea3ee8cf5e098
iAmInAction <83808704+iAmInActions@users.noreply.github.com>
2021-05-13 13:43:11 +0200

    Update gettop.sh

--- gettop.sh
+++ gettop.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd /root
+
+# Launch script goes here:
 wget https://raw.githubusercontent.com/iAmInActions/UsefullScripts/main/webdesktop.sh && chmod +x ./webdesktop.sh && ./webdesktop.sh
